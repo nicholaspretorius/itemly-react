4cf6af84923f97f458ebf13c1133d81fc3bc7ae0
Nicholas Pretorius <nicholaspretorius@gmail.com>
2018-12-25 10:55:41 +0200

    Sorted out webpack/babel.

---
+++ webpack.config.babel.js
@@ -0,0 +1,30 @@
+import path from 'path';
+
+export default {
+    mode: 'development',
+    context: path.resolve(__dirname),
+    entry: {
+        app: './src'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            include: [path.join(__dirname,'src')]
+        },{
+            test: /\.css/,
+            loader: 'style-loader!css-loader',
+        }]
+    },
+    devServer: {
+        inline:true,
+    }
+}
